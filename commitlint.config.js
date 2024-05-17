7ae03d3a8176f22667e31e84c0ca295083643bb1
GHR <ghr@GHR.local>
2024-05-17 18:45:54 +0200

    feat(houskeepingg): Add husky and commit linting

---
+++ commitlint.config.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(\w*)\((\w*)\): (.*)$/,
+            headerCorrespondence: ['type', 'scope', 'subject']
+        }
+    },
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'refactor',
+                'chore',
+                'test',
+                'docs',
+                'style'
+            ]
+        ],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'general',
+                'housekeeping',
+                'home-page',
+                'project',
+                'blog'
+            ]
+        ],
+        'scope-empty': [2, 'never']
+    }
+};
