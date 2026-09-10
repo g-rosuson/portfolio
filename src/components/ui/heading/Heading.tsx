aeea0386805848b43087050195f8938c83b15fe3
GHR <g.rosuson@gmail.com>
2026-09-10 15:51:13 +0200

    chore(housekeeping): improve heading and logo comps

--- src/components/ui/heading/Heading.tsx
+++ src/components/ui/heading/Heading.tsx
@@ -7,33 +7,15 @@ type Props = {
     level: 1 | 2 | 3;
     children: ReactNode;
     removeMargin?: boolean;
+    color?: 'yellow';
 }
 
-const Heading = ({ size = 'xl', level, children, removeMargin }: Props) => {
-    // Determine the heading classname based on the provided size
-    let sizeClass = styling.extraLarge;
-
-    if (size === 'l') {
-        sizeClass = styling.large;
-    }
-
-    if (size === 'm') {
-        sizeClass = styling.medium;
-    }
-
-    if (size === 's') {
-        sizeClass = styling.small;
-    }
-
-    if (size === 'xs') {
-        sizeClass = styling.extraSmall;
-    }
-
+const Heading = ({ size = 'xl', level, children, removeMargin, color }: Props) => {
     // Determine the heading element based on the provided level
     const Tag = `h${level}` as keyof React.JSX.IntrinsicElements;
 
     return (
-        <Tag className={sizeClass} data-remove-margin={!!removeMargin}>
+        <Tag className={styling.base} data-color={color} data-size={size} data-remove-margin={!!removeMargin}>
             {children}
         </Tag>
     );
